b6cc8c0da7cdfbc12726eece6079063a975f03fb
connikll <sautievumar77@gmail.com>
2025-12-11 15:49:40 +0300

    Fix count implementation

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
+
         String cmd = null;
         String text = null;
         Integer id = null;
@@ -24,52 +25,16 @@ public class App {
             System.out.println("Added #" + newId);
         }
 
+
         else if ("list".equals(cmd)) {
             var notes = store.loadNotes();
-            if (notes.isEmpty()) {
-                System.out.println("(empty)");
-                return;
-            }
             for (String[] n : notes) {
                 System.out.println(n[0] + ";" + n[1]);
             }
         }
 
-        package com.example;
-
-public class App {
-
-    public static void main(String[] args) {
-        String cmd = null;
-        String text = null;
-        Integer id = null;
-
-        for (String arg : args) {
-            if (arg.startsWith("--cmd=")) cmd = arg.substring(6);
-            if (arg.startsWith("--text=")) text = arg.substring(7);
-            if (arg.startsWith("--id=")) id = Integer.parseInt(arg.substring(5));
-        }
-
-        NotesStore store = new NotesStore();
-
-        if ("add".equals(cmd)) {
-            if (text == null) {
-                System.out.println("No text provided");
-                return;
-            }
-            int newId = store.add(text);
-            System.out.println("Added #" + newId);
-        }
-
-        else if ("list".equals(cmd)) {
-            var notes = store.loadNotes();
-            if (notes.isEmpty()) {
-                System.out.println("(empty)");
-                return;
-            }
-            for (String[] n : notes) {
-                System.out.println(n[0] + ";" + n[1]);
-            }
+        else if ("count".equals(cmd)) {
+            System.out.println(store.count());
         }
 
         else if ("rm".equals(cmd)) {
@@ -78,31 +43,9 @@ public class App {
                 return;
             }
             boolean ok = store.remove(id);
-            if (!ok) System.out.println("Not found #" + id);
-        }
-
-        else if ("count".equals(cmd)) {
-            System.out.println(store.count());
-        }
-
-        else {
-            System.out.println("Unknown command");
-        }
-    }
-}
-
-
-        else if ("rm".equals(cmd)) {
-            if (id == null) {
-                System.out.println("No id provided");
-                return;
+            if (!ok) {
+                System.out.println("Not found #" + id);
             }
-            boolean ok = store.remove(id);
-            if (!ok) System.out.println("Not found #" + id);
-        }
-
-        else if ("count".equals(cmd)) {
-            System.out.println(store.count());
         }
 
         else {
